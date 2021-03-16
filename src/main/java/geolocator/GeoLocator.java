e6e824f0972899f26fe3bc113355d90e17f7f70c
Fengchuanyu4437 <ffccyy4437@foxmail.com>
2021-03-17 03:51:50 +0800

    fix a problem

--- src/main/java/geolocator/GeoLocator.java
+++ src/main/java/geolocator/GeoLocator.java
@@ -12,25 +12,27 @@ import com.google.common.net.UrlEscapers;
 import org.apache.commons.io.IOUtils;
 
 /**
- * Class for obtaining geolocation information of an IP address. The class uses
- * the <a href="http://ip-api.com/">IP-API.com</a> service.
+ * Class for obtaining geolocation information of an IP address
+ * the class uses the <a href="https://ip-api.com/">IP-API.com</a>IP-API.com service
  */
+
 public class GeoLocator {
 
     /**
-     * URI of the geolocation service.
+     * UPI of the geolocation service
      */
+
     public static final String GEOLOCATOR_SERVICE_URI = "http://ip-api.com/json/";
 
     private static ObjectMapper OBJECT_MAPPER = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
     /**
-     * Creates a {@code GeoLocator} object.
+     * Creates a {@code Geolocation} object.
      */
     public GeoLocator() {}
 
     /**
-     * Returns geolocation information about the JVM running the application.
+     * Returns geolocation information about the JVM running the application
      *
      * @return an object wrapping the geolocation information returned
      * @throws IOException if any I/O error occurs
@@ -40,13 +42,13 @@ public class GeoLocator {
     }
 
     /**
-     * Returns geolocation information about the IP address or host name specified.
-     * If the argument is {@code null}, the method returns geolocation information
-     * about the JVM running the application.
+     * Returns geolocation information about the IP address or host name specified
+     *If the argument is {@code null} , the method returns geolocation information
+     * about the JVM running the application
      *
-     * @param ipAddrOrHost the IP address or host name, may be {@code null}
-     * @return an object wrapping the geolocation information returned
-     * @throws IOException if any I/O error occurs
+     * @param ipAddrOrHost an IP address or host , maybe {@code null}
+     * @return an object wrapping the geolocation information
+     * @throws IOException IF I/O errors occurs
      */
     public GeoLocation getGeoLocation(String ipAddrOrHost) throws IOException {
         URL url;
@@ -59,8 +61,7 @@ public class GeoLocator {
         String s = IOUtils.toString(url, "UTF-8");
         return OBJECT_MAPPER.readValue(s, GeoLocation.class);
     }
-
-    // CHECKSTYLE:OFF
+    // CHECKSTYLE: OFF
     public static void main(String[] args) throws IOException {
         try {
             String arg = args.length > 0 ? args[0] : null;
@@ -69,5 +70,5 @@ public class GeoLocator {
             System.err.println(e.getMessage());
         }
     }
-
+// CHECKSTYLE: ON
 }
